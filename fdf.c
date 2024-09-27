b015c96315811a7207e3bb29f3776e3973cccfb3
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-27 15:33:06 +0100

    Open a window with the mlx library

--- fdf.c
+++ fdf.c
@@ -6,15 +6,19 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:09:58 by ansebast          #+#    #+#             */
-/*   Updated: 2024/09/27 15:14:08 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:32:31 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-
 int	main(void)
 {
-	ft_putstr_fd("Teste", 1);
-        return (0);
+	void	*mlx;
+        // void	*mlx_window;
+
+	mlx = mlx_init();
+        mlx_new_window(mlx, 1080, 920, "Hello, Word!");
+        mlx_loop(mlx);
+	return (0);
 }
