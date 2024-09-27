de502bfcfbaca5e8592f62595072f16588b8bb6a
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-27 15:28:39 +0100

    Test if the project is running

---
+++ fdf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 13:09:55 by ansebast          #+#    #+#             */
+/*   Updated: 2024/09/27 15:13:41 by ansebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include "libft/libft.h"
+# include "minilibx/mlx.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
