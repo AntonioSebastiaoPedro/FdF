43b700c497b0872eae033d3b3f86f2dbbd0c9792
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-09-28 11:10:42 +0100

    Adjust the scale depending on the height and width of the map

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:09:58 by ansebast          #+#    #+#             */
-/*   Updated: 2024/09/28 10:57:24 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/09/28 11:04:12 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -250,15 +250,15 @@ int	main(int ac, char **av)
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
-	scale = fmin(1920 / width, 1080 / height);
+	scale = fmin(1920 / width / 2, 1080 / height / 2);
         if (scale <= 0)
                 scale = 1;
-        else if (scale > 20)
+        else if (scale > 10)
                 scale /= 2;
-        // x_offset = (1920 - (width * scale)) / 2;
-        // y_offset = (1080 - (height * scale)) / 2;
-        x_offset = (1920 / 2) - ((width * scale) / 2);
-	y_offset = (1080 / 2) - ((height * scale) / 2);
+        x_offset = (1920 - (width * scale)) / 2 - 100;
+        y_offset = (1080 - (height * scale)) / 2 + 200;
+        // x_offset = (1920 / 2) - ((width * scale) / 2);
+	// y_offset = (1080 / 2) - ((height * scale) / 2);
 	draw_map(&img, map, height, width, scale, x_offset, y_offset);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
