737c9abca24b47a5055665d29dd8e2eec54766c9
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-10-01 19:27:41 +0100

    Add functions to update min and max x,y points

--- projection.c
+++ projection.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:03:36 by ansebast          #+#    #+#             */
-/*   Updated: 2024/10/01 18:06:18 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/10/01 19:26:17 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,37 @@
 t_point	project_point(int x, int y, int z, int color, t_vars *vars)
 {
 	t_point	proj;
+
 	if (vars->rotate == 1)
-                rotate_z(x, y, z, vars, &proj);
-        else if (vars->rotate == 2)
-                rotate_x(x, y, z, vars, &proj);
-        else if (vars->rotate == 3)
-                rotate_y(x, y, z, vars, &proj);
+		rotate_z(x, y, z, vars, &proj);
+	else if (vars->rotate == 2)
+		rotate_x(x, y, z, vars, &proj);
+	else if (vars->rotate == 3)
+		rotate_y(x, y, z, vars, &proj);
 	proj.z = z;
 	proj.color = color;
 	return (proj);
+}
+
+void	update_bounds(int *first, t_bounds *bounds, t_point *point)
+{
+	if (*first)
+	{
+		bounds->min_x = bounds->max_x = point->x;
+		bounds->min_y = bounds->max_y = point->y;
+		*first = 0;
+	}
+	else
+	{
+		if (point->x < bounds->min_x)
+			bounds->min_x = point->x;
+		if (point->x > bounds->max_x)
+			bounds->max_x = point->x;
+		if (point->y < bounds->min_y)
+			bounds->min_y = point->y;
+		if (point->y > bounds->max_y)
+			bounds->max_y = point->y;
+	}
 }
 
 t_bounds	get_projected_bounds(t_vars *vars)
@@ -31,35 +53,24 @@ t_bounds	get_projected_bounds(t_vars *vars)
 	t_bounds	bounds;
 	t_point		p;
 	int			first;
+	int			x;
+	int			y;
 
-	int x, y;
 	first = 1;
-	for (y = 0; y < vars->height; y++)
+	y = 0;
+	while (y < vars->height)
 	{
-		for (x = 0; x < vars->width; x++)
+		x = 0;
+		while (x < vars->width)
 		{
 			if (!vars->map[y] || !vars->map[y][x])
 				continue ;
 			p = project_point(x, y, vars->map[y][x][0], vars->map[y][x][1],
 					vars);
-			if (first)
-			{
-				bounds.min_x = bounds.max_x = p.x;
-				bounds.min_y = bounds.max_y = p.y;
-				first = 0;
-			}
-			else
-			{
-				if (p.x < bounds.min_x)
-					bounds.min_x = p.x;
-				if (p.x > bounds.max_x)
-					bounds.max_x = p.x;
-				if (p.y < bounds.min_y)
-					bounds.min_y = p.y;
-				if (p.y > bounds.max_y)
-					bounds.max_y = p.y;
-			}
+			update_bounds(&first, &bounds, &p);
+			x++;
 		}
+		y++;
 	}
 	return (bounds);
 }
