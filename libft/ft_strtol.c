c371a74a935f2c6c45f81a0ca692070099c45957
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-10-03 16:08:33 +0100

    Fix ft_strtol function

--- libft/ft_strtol.c
+++ libft/ft_strtol.c
@@ -6,28 +6,69 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 14:27:24 by ansebast          #+#    #+#             */
-/*   Updated: 2024/10/03 15:15:24 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/10/03 16:06:40 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long	ft_strtol(char *str, char **endptr, int base)
+static int	ft_get_base(char **s, int base)
 {
-	int		i;
-	int		signal;
-	long	num;
+	if (base == 0)
+	{
+		if (**s == '0')
+		{
+			if ((*s)[1] == 'x' || (*s)[1] == 'X')
+			{
+				*s += 2;
+				return (16);
+			}
+			else
+			{
+				(*s)++;
+				return (8);
+			}
+		}
+		return (10);
+	}
+	else if (base == 16 && **s == '0' && ((*s)[1] == 'x' || (*s)[1] == 'X'))
+		*s += 2;
+	return (base);
+}
 
-	signal = 1;
-	num = 0;
-	i = 0;
-	while (ft_isspace(str[i]))
-		i++;
-	if (str[i] == '-')
-		signal = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	ft_convert_digits(str, &i, &num, base);
-	*endptr = &str[i];
-	return (num * signal);
+static int	ft_get_digit(char c)
+{
+	if (ft_isdigit(c))
+		return (c - '0');
+	else if (ft_isalpha(c))
+		return (ft_toupper(c) - 'A' + 10);
+	return (-1);
+}
+
+long	ft_strtol(char *s, char **endptr, int base)
+{
+	long	result;
+	int		sign;
+	int		digit;
+
+	result = 0;
+	sign = 1;
+	while (ft_isspace(*s))
+		s++;
+	if (*s == '-')
+		sign = -1;
+	if (*s == '-' || *s == '+')
+		s++;
+	base = ft_get_base(&s, base);
+	while (*s)
+	{
+		digit = ft_get_digit(*s);
+		if (digit < 0 || digit >= base)
+			break ;
+		result = result * base + digit;
+		s++;
+	}
+	if (endptr)
+		*endptr = (char *)(s);
+	return (result * sign);
 }
