af1cffdf924476a90b428ee539a2889c2e778a70
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-10-05 12:41:41 +0100

    Validate file

--- validate_file.c
+++ validate_file.c
@@ -6,44 +6,25 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 10:48:09 by ansebast          #+#    #+#             */
-/*   Updated: 2024/10/05 12:15:31 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/10/05 12:38:02 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <stdio.h>
 
 void	check_file(char *path_file)
 {
 	char	*extension;
 	int		len;
 
+	len = len_line_file(path_file);
 	extension = ft_strrchr(path_file, '.');
 	if (!extension || ft_strncmp(extension, ".fdf", 4))
 		ft_puterror("Invalid file.\n", 1);
-	len = len_line_file(path_file);
 	if (len == -1)
 		ft_puterror("Error opening file.\n", 1);
 	if (len == 0)
 		ft_puterror("The file is empty.\n", 1);
-	if (check_wrong_lines(path_file))
-		ft_puterror("Found wrong line length.\n", 1);
-}
-
-int	ft_countarray(char **array)
-{
-	int	len;
-
-	len = 0;
-	if (!array)
-		return (len);
-	while (array[len])
-	{
-		free(array[len]);
-		len++;
-	}
-	free(array);
-	return (len);
 }
 
 int	len_line_file(char *path_file)
@@ -65,31 +46,4 @@ int	len_line_file(char *path_file)
 	}
 	close(fd);
 	return (len);
-}
-
-int	check_wrong_lines(char *path_file)
-{
-	int		fd;
-	int		len_line;
-	char	*line;
-	char	**spl;
-
-	fd = open(path_file, O_RDONLY);
-	line = get_next_line(fd);
-	spl = ft_split(line, ' ');
-	len_line = ft_countarray(spl);
-	while (line)
-	{
-		spl = ft_split(line, ' ');
-		if (len_line != ft_countarray(spl))
-		{
-			free(line);
-			close(fd);
-			return (1);
-		}
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
 }
