840358033e6b6ed9c5f3a83a479bb71b6f19e1b9
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-10-05 20:21:47 +0100

    Adjust the scale size if it is too large

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:54:08 by ansebast          #+#    #+#             */
-/*   Updated: 2024/10/04 23:09:26 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/10/05 20:13:06 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	calculate_scale(t_vars *vars)
 	scale_x = (double)WIN_WIDTH / ((bounds.max_x) - (bounds.min_x));
 	scale_y = (double)(WIN_HEIGHT - 220) / (bounds.max_y - bounds.min_y);
 	vars->scale = fmin(scale_x, scale_y);
+	if (vars->scale > 300)
+		vars->scale = 300;
 	bounds = get_projected_bounds(vars);
 	vars->x_offset = (WIN_WIDTH - (bounds.max_x - bounds.min_x)) / 2
 		- bounds.min_x;
